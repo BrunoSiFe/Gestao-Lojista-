8fd56248652c511eb705838a24e0ab3e2abe0f3e
BrunoSilva <BrunoSiFe@gmail.com>
2018-08-08 18:05:43 -0300

    Inicio do Projeto

---
+++ Gerencia Lojista/src/main/java/br/com/bruno/gerencia/lojista/Gerencia/Lojista/GerenciaLojistaApplication.java
@@ -0,0 +1,12 @@
+package br.com.bruno.gerencia.lojista.Gerencia.Lojista;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciaLojistaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciaLojistaApplication.class, args);
+	}
+}
